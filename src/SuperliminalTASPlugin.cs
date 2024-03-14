5e14644894a0ab8801f1087d7a7c5069b52a0a0b
Micrologist <7757920+Micrologist@users.noreply.github.com>
2024-03-14 02:30:47 +0100

    remove leftover code

--- src/SuperliminalTASPlugin.cs
+++ src/SuperliminalTASPlugin.cs
@@ -24,32 +24,6 @@ public class SuperliminalTASPlugin : BaseUnityPlugin
 		Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 		TimeManagerPatcher.Patch(Process.GetCurrentProcess());
 		this.gameObject.AddComponent<DemoRecorder>();
-
-		var extensionList = new[] {
-			new SFB.ExtensionFilter("Superliminal TAS Recording (*.slt)", "slt"),
-			new SFB.ExtensionFilter("All Files", "*")
-		};
-		/**
-		StandaloneFileBrowserWindows standaloneFileBrowserWindows = new StandaloneFileBrowserWindows();
-		var saveLocation = standaloneFileBrowserWindows.SaveFilePanel("Save Recording as", Application.dataPath, $"SuperliminalTAS-{System.DateTime.Now:yyyy-MM-dd-HH-mm-ss}.slt", extensionList);
-		Logger.LogInfo(saveLocation);
-		
-		
-		var file = File.OpenWrite(saveLocation.Name);
-
-		using (StreamWriter writer = new StreamWriter(file))
-		{
-			writer.WriteLine("TestABC");
-		}
-		**/
-
-		
-	}
-
-	private bool ConfirmOverwrite(string fileName)
-	{
-		return MessageBox.Show($"{fileName} already exists.\nDo you want to replace it?", "Confirm Save As",
-	MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes;
 	}
 }
 
