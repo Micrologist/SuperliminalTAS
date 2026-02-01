897b8ec91ff628c6d42b442c069ed675f44d21b8
Micrologist <7757920+Micrologist@users.noreply.github.com>
2026-02-01 20:52:44 +0100

    merge latest fixes back in (#13)

--- SuperliminalToolsPlugin.cs
+++ SuperliminalToolsPlugin.cs
@@ -7,10 +7,9 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using UnityEngine;
-
+using BepInEx.Logging;
 
 #if LEGACY
-using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 #endif
 
@@ -18,66 +17,54 @@ namespace SuperliminalTools;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 #if LEGACY
-public class SuperliminalToolsPlugin : BasePlugin
+public partial class SuperliminalToolsPlugin : BasePlugin
 {
     internal static new ManualLogSource Log;
 
     public override void Load()
     {
         Log = base.Log;
-
-        var args = Environment.GetCommandLineArgs();
-        bool tasMode = args.Contains("--tas");
-
-        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded! Mode: {(tasMode ? "TAS" : "Practice")}");
-
-        // Create a persistent GameObject that survives scene transitions
-        var go = new GameObject("SuperliminalTools");
-        UnityEngine.Object.DontDestroyOnLoad(go);
-        go.hideFlags = HideFlags.HideAndDontSave;
-
-        SuperliminalTools.Components.Utility.RegisterAllIL2CPPTypes();
-        SuperliminalTools.Components.Utility.AddAllSharedComponents(go);
-        var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-
-        if (tasMode)
-        {
-            go.AddComponent<TASModController>();
-            UnityEngineTimePatcher.Patch(Process.GetCurrentProcess());
-            harmony.PatchAll();
-        }
-        else
-        {
-            go.AddComponent<PracticeModController>();
-            harmony.PatchAll(typeof(LerpPlayerMantlePatch));
-            harmony.PatchAll(typeof(SaveGamePatch));
-            harmony.PatchAll(typeof(NormalLoadingScreensPatch));
-            harmony.PatchAll(typeof(DisableAlarmSoundPatch));
-#if HAS_WARNING_CONTROLLER
-            harmony.PatchAll(typeof(DisableWarningScreenPatch));
-#endif
-            harmony.PatchAll(typeof(DontPauseOnLostFocusPatch));
-            harmony.PatchAll(typeof(LegacyResetCheckpointPatch));
-            harmony.PatchAll(typeof(HotCofeeErrorPatch));
-
-        }
+        Initialize();
     }
 }
 #else
-public class SuperliminalToolsPlugin : BaseUnityPlugin
+public partial class SuperliminalToolsPlugin : BaseUnityPlugin
 {
+    internal static ManualLogSource Log;
+
     private void Awake()
+    {
+        Log = Logger;
+        Initialize();
+    }
+}
+#endif
+
+public partial class SuperliminalToolsPlugin
+{
+    private void Initialize()
     {
         var args = Environment.GetCommandLineArgs();
         bool tasMode = args.Contains("--tas");
 
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded! Mode: {(tasMode ? "TAS" : "Practice")}");
+        var targetVersion = MyPluginInfo.PLUGIN_GUID.Replace(".superliminaltools", "");
+
+        if(Application.version.IndexOf(targetVersion) < 0)
+        {
+            Log.LogError($"Plugin {MyPluginInfo.PLUGIN_GUID} targets {targetVersion} but game version is {Application.version}.");
+            return;
+        }
+
+        Log.LogInfo($"{MyPluginInfo.PLUGIN_GUID} is loaded! Mode: {(tasMode ? "TAS" : "Practice")}");
 
         // Create a persistent GameObject that survives scene transitions
         var go = new GameObject("SuperliminalTools");
         UnityEngine.Object.DontDestroyOnLoad(go);
         go.hideFlags = HideFlags.HideAndDontSave;
 
+#if LEGACY
+        SuperliminalTools.Components.Utility.RegisterAllIL2CPPTypes();
+#endif
         SuperliminalTools.Components.Utility.AddAllSharedComponents(go);
         var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
 
@@ -98,7 +85,10 @@ public class SuperliminalToolsPlugin : BaseUnityPlugin
             harmony.PatchAll(typeof(DisableWarningScreenPatch));
 #endif
             harmony.PatchAll(typeof(DontPauseOnLostFocusPatch));
+#if LEGACY
+            harmony.PatchAll(typeof(LegacyResetCheckpointPatch));
+            harmony.PatchAll(typeof(HotCofeeErrorPatch));
+#endif
         }
     }
 }
-#endif
